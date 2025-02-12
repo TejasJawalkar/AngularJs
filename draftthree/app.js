ab3cf470fa30dbf275807e9bb03d023f702f5fe2
Tejas Jawalkar <tejassj1998@gmail.com>
2025-02-12 10:10:04 +0530

    ecommerce project created

--- draftthree/app.js
+++ draftthree/app.js
@@ -3,21 +3,166 @@
 
 var app=angular.module("loginModule",[])
 app.controller("loginController",function($scope){
-
     //defining model
-    $scope.user={
-        email:"xyz@gmail.com",
-        password:"1234"
-    }
 
-    $scope.valiate=function(){
-        if($scope.user.email=="xyz@gmail.com" && $scope.user.password=="1234")
-        {
-            console.log("Welcome User");
-        }
-        else
-        {
-            console.log("Login Failed");
-        }
+    // $scope.userList = 
+    // [
+    //     {
+    //         name: "john sharma",
+    //         email: "john@gmail.com",
+    //         code: "E1"
+    //     },
+    //     {
+    //         name: "peter ganguli",
+    //         email: "peter@gmail.com",
+    //         code: "E2"
+    //     },
+    //     {
+    //         name: "subhodh pant",
+    //         email: "subhodh@gmail.com",
+    //         code: "E3"
+    //     }
+    // ]
+
+    // $scope.user={
+    //     email:"",
+    //     password:""
+    // }
+
+    // $scope.valiate=function(){
+    //     if($scope.user.email=="xyz@gmail.com" && $scope.user.password=="1234")
+    //     {
+    //        document.getElementById("userlist").style.display="block";
+    //     }
+    //     else
+    //     {
+    //         document.getElementById("userlist").style.display="none";
+    //     }
+    // }
+
+    $scope.productsList=[
+    {
+        name:"Iphone 15",
+        price:"54,000",
+        processor:"IOS",
+        brand:"Apple",
+        url:"https://cdn.shopify.com/s/files/1/0376/5420/0459/files/BA_ALAK_200.1001_2381f096-6ff8-4260-aac1-9cfc3faf411d.png?v=1693208232"
+    },
+    {
+        name:"Samsung 25",
+        price:"75,000",
+        processor:"Snapdragon",
+        brand:"Samsung",
+        url:"https://cdn.shopify.com/s/files/1/0376/5420/0459/files/BA_ALAK_200.1001_2381f096-6ff8-4260-aac1-9cfc3faf411d.png?v=1693208232"
+    },
+    {
+        name:"Nothing 2a",
+        processor:"Dimensity",
+        price:"24,000",
+        brand:"Nothing",
+        url:"https://cdn.shopify.com/s/files/1/0376/5420/0459/files/BA_ALAK_200.1001_2381f096-6ff8-4260-aac1-9cfc3faf411d.png?v=1693208232"
+    },
+    {
+        name:"Realme 13 Pro",
+        processor:"Dimensity",
+        price:"20,000",
+        brand:"Realme",
+    },
+    {
+        name:"POCO F7",
+        processor:"Snapdragon",
+        price:"27,000",
+        brand:"POCO",
+    },
+    {
+        name:"Iphone 15",
+        price:"54,000",
+        processor:"IOS",
+        brand:"Apple",
+    },
+    {
+        name:"Samsung 25",
+        price:"75,000",
+        processor:"Snapdragon",
+        brand:"Samsung",
+    },
+    {
+        name:"Nothing 2a",
+        processor:"Dimensity",
+        price:"24,000",
+        brand:"Nothing",
+    },
+    {
+        name:"Realme 13 Pro",
+        processor:"Dimensity",
+        price:"20,000",
+        brand:"Realme",
+    },
+    {
+        name:"POCO F7",
+        processor:"Snapdragon",
+        price:"27,000",
+        brand:"POCO",
+    },
+    {
+        name:"Iphone 15",
+        price:"54,000",
+        processor:"IOS",
+        brand:"Apple",
+    },
+    {
+        name:"Samsung 25",
+        price:"75,000",
+        processor:"Snapdragon",
+        brand:"Samsung",
+    },
+    {
+        name:"Nothing 2a",
+        processor:"Dimensity",
+        price:"24,000",
+        brand:"Nothing",
+    },
+    {
+        name:"Realme 13 Pro",
+        processor:"Dimensity",
+        price:"20,000",
+        brand:"Realme",
+    },
+    {
+        name:"POCO F7",
+        processor:"Snapdragon",
+        price:"27,000",
+        brand:"POCO",
+    },
+    {
+        name:"Iphone 15",
+        price:"54,000",
+        processor:"IOS",
+        brand:"Apple",
+    },
+    {
+        name:"Samsung 25",
+        price:"75,000",
+        processor:"Snapdragon",
+        brand:"Samsung",
+    },
+    {
+        name:"Nothing 2a",
+        processor:"Dimensity",
+        price:"24,000",
+        brand:"Nothing",
+    },
+    {
+        name:"Realme 13 Pro",
+        processor:"Dimensity",
+        price:"20,000",
+        brand:"Realme",
+    },
+    {
+        name:"POCO F7",
+        processor:"Snapdragon",
+        price:"27,000",
+        brand:"POCO",
     }
+]
 })
---
+++ ecommerce/modules/auth.module.js
@@ -0,0 +1,8 @@
+function authentication(email,password)
+{
+    let status=false;
+    if (email == "xyz@gmail.com" && password == "1234") {
+        status=true;
+    }
+    return status;
+}
